cdf4b24e9c019f3f336bcb0055a0f9eee4a04360
yogi <moh.yogi10065@gmail.com>
2022-12-28 23:14:24 +0700

    add migration

---
+++ migrations/20221228155140-users.js
@@ -0,0 +1,57 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+   
+    await queryInterface.createTable('users', {
+       id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      name:{
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      gender:{
+        type: Sequelize.ENUM('laki-laki', 'wanita'),
+        allowNull: false
+      },
+      age:{
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role_id:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at:{
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at:{
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+      });
+
+      await queryInterface.addConstraint('users',{
+        type: 'foreign key',
+        name: 'role_id_users',
+        fields:['role_id'],
+        references:{
+          table: 'role',
+          field: 'id'
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
+      });
+    
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
