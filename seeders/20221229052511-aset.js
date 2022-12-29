7f8bf25102185b3752c94ef26436977d577f0dd6
yogi <moh.yogi10065@gmail.com>
2022-12-29 18:00:44 +0700

    create new data seeder

--- seeders/20221229052511-aset.js
+++ seeders/20221229052511-aset.js
@@ -1,13 +1,16 @@
 'use strict';
 
+
+// TODO 
+// Must using Pivot table
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
     
       await queryInterface.bulkInsert('asets', [
         {
-          name: null,
-          user_id: 1,
+          name: 'iPhone 9',
+          user_id: 6,
           created_at: new Date(),
           updated_at: new Date(),
         },
@@ -18,11 +21,69 @@ module.exports = {
           updated_at: new Date(),
         },
         {
-          name: null,
+          name: 'Samsung Galaxy Book',
+          user_id:2,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'iPhone X',
+          user_id:7,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Huawei P30',
           user_id:3,
           created_at: new Date(),
           updated_at: new Date(),
         },
+        {
+          name: 'Samsung Universe 9',
+          user_id:8,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Huawei P30',
+          user_id:9,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'iPhone X',
+          user_id:9,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Samsung Universe 9',
+          user_id:4,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Samsung Galaxy Book',
+          user_id:10,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Huawei P30',
+          user_id:5,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'iPhone X',
+          user_id:11,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+
+        
+        
+        
 
     ], {});
     
